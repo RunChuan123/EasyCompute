47db86aa4b4c7c5628b3ac4b630c8f17bd5b7e33
RunChuan123 <3233096165@qq.com>
2026-03-22 22:46:00 +0800

    i

---
+++ src/device/cuda/kernel/gemm.cu
@@ -0,0 +1,54 @@
+#include <cuda_runtime.h>
+
+// 每个线程计算 C 中一个元素
+__global__ void gemm_f32_v1(float alpha,float* A,float* B,float beta,float* C,int M,int K,int N){
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float sum = 0.0f;
+
+    for(int i = 0; i< K;i++){
+        sum += A[ty*K + i] * B[i*N + tx];
+    }
+    C[ty*N + tx] = alpha * sum + beta * C[ty * N + tx];
+}
+
+// block 分块计算
+template<const size_t BLOCK_SIZE>
+__global__ void gemm_f32_v2(float alpha,float* A,float* B,float beta,float* C,int M,int K,int N){
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    const int BM = BLOCK_SIZE;
+    const int BN = BLOCK_SIZE;
+    const int BK = BLOCK_SIZE;
+
+    int tx = threadIdx.x % BN;
+    int ty = threadIdx.x / BN;
+
+    __shared__ float As[BM*BK];
+    __shared__ float Bs[BK*BN];
+
+    A = &A[by*K *BK];
+    B = &B[bx * BN];
+    C = &C[by*N*BK + bx * BN];
+
+    float tmp = 0;
+
+    for(size_t i = 0;i<K;i+=BK){
+        As[ty*BK + tx] = A[ty*K+tx];
+        Bs[ty * BN + tx] = B[ty * N + tx];
+        __syncthreads();
+        A += BK;
+        B += BK * N;
+        for(size_t j=0;j < BK;j++){
+            tmp += As[ty*BK+j] * Bs[BN * j + tx];
+        }
+        __syncthreads();
+    }
+    C[ty*N+tx] = alpha* tmp + beta * C[ty*N+tx];
+}
+
+__global__ void gemm_f32_v3(){
+    
+}
---
+++ src/device/cuda/kernel/test/test_g.cu
@@ -0,0 +1,61 @@
+#include <cuda_runtime.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include "../elementwise.cu"
+#include "../gemm.cu"
+
+// __global__ void element_wise_add_f32x4_full_impl(float* a,float* b,float* out,size_t N);
+static void checkCuda(cudaError_t err, const char* msg) {
+    if (err != cudaSuccess) {
+        std::cerr << msg << ": " << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    const int n = 1024;
+    const size_t bytes = n * sizeof(float);
+    float va = 1.0f;
+    float vb = 2.0f;
+    float vc = 2.0f;
+    std::vector<float> h_a(n, va); // 32 * 32
+    std::vector<float> h_b(n, vb);
+    std::vector<float> h_c(n,vc);
+
+    float *d_a = nullptr, *d_b = nullptr, *d_c = nullptr;
+
+    checkCuda(cudaMalloc(&d_a, bytes), "cudaMalloc d_a failed");
+    checkCuda(cudaMalloc(&d_b, bytes), "cudaMalloc d_b failed");
+    checkCuda(cudaMalloc(&d_c, bytes), "cudaMalloc d_c failed");
+
+    checkCuda(cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice), "copy a failed");
+    checkCuda(cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice), "copy b failed");
+    checkCuda(cudaMemcpy(d_c, h_c.data(), bytes, cudaMemcpyHostToDevice), "copy b failed");
+
+    dim3 block(16 * 16);
+    dim3 grid((32 + 16 - 1) / 16, (32 + 16 - 1) / 16);  // (2, 2)
+    float alpha = 1.0f;
+    float beta = 2.0f;
+    gemm_f32_v2<16><<<grid, block>>>(alpha,d_a, d_b,beta, d_c,32,32,32);
+
+    checkCuda(cudaGetLastError(), "kernel launch failed");
+    checkCuda(cudaDeviceSynchronize(), "kernel sync failed");
+
+    checkCuda(cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost), "copy c failed");
+
+    for (int i = 0; i < n; ++i) {
+        // std::cout << h_c[i];
+        if (h_c[i] != va*vb * alpha * 32 + beta * vc){
+            throw std::runtime_error("mismatch");
+        } 
+        
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    std::cout << "test_add_kernel passed\n";
+    return EXIT_SUCCESS;
+}
